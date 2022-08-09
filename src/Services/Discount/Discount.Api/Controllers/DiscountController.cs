4a803cbb02320d335939f371a59bbecb11c7a3f1
shrikant@flexli.in <shrikant@flexli.in>
2022-08-09 18:09:08 +0530

    Add Discount WebApi Project

---
+++ src/Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.Api.Controllers;
+
+public class DiscountController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
